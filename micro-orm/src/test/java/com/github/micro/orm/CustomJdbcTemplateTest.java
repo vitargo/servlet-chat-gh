2ee4241747a52dc89b85440fdffb10c4000b3a4e
ShyrmaOV <alex1998sh.com@gmail.com>
2021-04-12 13:44:17 +0300

    refactor test

--- micro-orm/src/test/java/com/github/micro/orm/CustomJdbcTemplateTest.java
+++ micro-orm/src/test/java/com/github/micro/orm/CustomJdbcTemplateTest.java
@@ -1,21 +1,20 @@
 package com.github.micro.orm;
 
 import com.github.chat.config.DatabaseConfig;
+import com.github.chat.entity.User;
 import com.github.micro.orm.impl.UserRowMapper;
 import org.junit.Test;
 
 import java.util.Collection;
 
-import static org.junit.Assert.*;
-
 public class CustomJdbcTemplateTest {
 
     CustomJdbcTemplate jdbcTemplate = new CustomJdbcTemplate(new DatabaseConfig());
-    UserRowMapper rowMapper;
+    UserRowMapper rowMapper = new UserRowMapper();
 
     @Test
     public void findAll() {
-        Collection act = jdbcTemplate.findAll("SELECT * FROM ", rowMapper);
+        Collection<User> act = jdbcTemplate.findAll("SELECT * FROM user", rowMapper);
         System.out.println(act + "jj");
     }
 
