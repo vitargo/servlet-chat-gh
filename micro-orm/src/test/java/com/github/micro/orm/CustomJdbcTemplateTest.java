cd1ff48aeddd6167b26f8175006fd02388a771f8
ShyrmaOV <alex1998sh.com@gmail.com>
2021-04-12 13:47:47 +0300

    refactor test

--- micro-orm/src/test/java/com/github/micro/orm/CustomJdbcTemplateTest.java
+++ micro-orm/src/test/java/com/github/micro/orm/CustomJdbcTemplateTest.java
@@ -5,6 +5,7 @@ import com.github.chat.entity.User;
 import com.github.micro.orm.impl.UserRowMapper;
 import org.junit.Test;
 
+import java.sql.SQLException;
 import java.util.Collection;
 
 import static org.junit.Assert.*;
@@ -16,7 +17,12 @@ public class CustomJdbcTemplateTest {
 
     @Test
     public void findAll() {
-        Collection act = jdbcTemplate.findAll("SELECT * FROM ", rowMapper);
+        Collection<User> act = null;
+        try {
+            act = jdbcTemplate.findAll("SELECT * FROM ", rowMapper);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         System.out.println(act + "jj");
     }
 
