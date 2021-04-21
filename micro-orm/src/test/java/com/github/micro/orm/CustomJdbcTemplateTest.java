cc1307dd9b69899cb1fc00b885fc19036af2bf75
Vita Redko <rvitargo@gmail.com>
2021-04-21 12:24:18 +0300

    add hiroku procfile

--- micro-orm/src/test/java/com/github/micro/orm/CustomJdbcTemplateTest.java
+++ micro-orm/src/test/java/com/github/micro/orm/CustomJdbcTemplateTest.java
@@ -1,21 +1,15 @@
 package com.github.micro.orm;
 
-import org.junit.Test;
-
-import java.util.Collection;
-
-import static org.junit.Assert.*;
-
 public class CustomJdbcTemplateTest {
 
-    CustomJdbcTemplate jdbcTemplate;
-    CustomRowMapper rowMapper;
-
-    @Test
-    public void findAll() {
-        Collection act = null;
-        act = jdbcTemplate.findAll("SELECT * FROM ", rowMapper);
-        System.out.println(act + "jj");
-    }
+//    CustomJdbcTemplate jdbcTemplate;
+//    CustomRowMapper rowMapper;
+//
+//    @Test
+//    public void findAll() {
+//        Collection act = null;
+//        act = jdbcTemplate.findAll("SELECT * FROM ", rowMapper);
+//        System.out.println(act + "jj");
+//    }
 
 }
