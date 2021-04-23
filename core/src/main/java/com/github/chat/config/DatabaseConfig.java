c483e9a872e9f8adce14f6dd74f8fe7657ba6c4c
ShyrmaOV <alex1998sh.com@gmail.com>
2021-04-23 16:51:37 +0300

    some change for DatabaseConfig

--- core/src/main/java/com/github/chat/config/DatabaseConfig.java
+++ core/src/main/java/com/github/chat/config/DatabaseConfig.java
@@ -3,9 +3,6 @@ package com.github.chat.config;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
 public class DatabaseConfig {
 
     private static HikariConfig config = new HikariConfig();
@@ -16,14 +13,11 @@ public class DatabaseConfig {
         config.setJdbcUrl( "jdbc:postgresql://localhost:5432/servlet_chat" );
         config.setUsername( "user" );
         config.setPassword( "password" );
-        config.addDataSourceProperty( "cachePrepStmts" , "true" );
-        config.addDataSourceProperty( "prepStmtCacheSize" , "250" );
-        config.addDataSourceProperty( "prepStmtCacheSqlLimit" , "2048" );
         dataSource = new HikariDataSource( config );
     }
 
-    public static Connection getConnection() throws SQLException {
-        return dataSource.getConnection();
+    public static HikariConfig getConfig() {
+        return config;
     }
 
     public static HikariDataSource getDataSource() {
