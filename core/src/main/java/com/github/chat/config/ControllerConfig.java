d11463935f3772bc7e46c57e75b3f446cb4eb0ee
ShyrmaOV <alex1998sh.com@gmail.com>
2021-05-10 20:31:40 +0300

    configure dataSource

--- core/src/main/java/com/github/chat/config/ControllerConfig.java
+++ core/src/main/java/com/github/chat/config/ControllerConfig.java
@@ -1,16 +1,20 @@
 package com.github.chat.config;
 
 import com.github.chat.controllers.UsersController;
+import com.github.chat.entity.User;
 import com.github.chat.repository.UsersRepository;
 import com.github.chat.repository.impl.UserRepoImpl;
 import com.github.chat.service.IUsersService;
 import com.github.chat.service.UserService;
+import com.github.micro.orm.CustomJdbcTemplate;
 
 public class ControllerConfig {
 
-    private static UsersRepository usersRepository = new UserRepoImpl(DatabaseConfig.getDataSource());
+    private static final CustomJdbcTemplate<User> customJdbcTemplate = new CustomJdbcTemplate<>(DatabaseConfig.getDataSource());
 
-    private static IUsersService usersService = new UserService(usersRepository);
+    private static final UsersRepository usersRepository = new UserRepoImpl(customJdbcTemplate);
+
+    private static final IUsersService usersService = new UserService(usersRepository);
 
     public static UsersController usersController(){
 
