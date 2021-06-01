71af76d098d6a798d6e336bdb3f5d0f448a4d289
Bamsteh <andrey20111998@gmail.com>
2021-06-02 01:54:15 +0300

    1

--- core/src/main/java/com/github/chat/ChatApplication.java
+++ core/src/main/java/com/github/chat/ChatApplication.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 public class ChatApplication {
 
     public static void main(String[] args) throws ServletException {
+        int i = 0;
         ServerConfig.start().run();
     }
 }
